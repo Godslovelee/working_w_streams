88476f9aaf3f60bf24bf7cbb11bb7a0f372a14e3
Godslovelee <godslovelee@yahoo.com>
2021-12-01 21:27:46 +0100

    "converted stream controller to broadcast stream"

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  StreamController<double> controller = StreamController();
+  StreamController<double> controller = StreamController<double>.broadcast();
 
   StreamSubscription<double>streamSubscription;
 
