10e83e3bee04d5ac8e70187a8c04005cc0162e97
Godslovelee <godslovelee@yahoo.com>
2021-12-01 22:15:38 +0100

    "creating a stream function"

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-
   //adding stream controller
   @override
   _MyAppState createState() => _MyAppState();
@@ -17,29 +16,27 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   StreamController<double> controller = StreamController<double>.broadcast();
 
-  StreamSubscription<double>streamSubscription;
+  StreamSubscription<double> streamSubscription;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: Center(
+            child: Row(mainAxisSize: MainAxisSize.min, children: [
+              MaterialButton(
+                child: Text("subscribe"),
+                color: Colors.brown,
+                onPressed: () {
 
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children:[ MaterialButton(
-              child: Text(
-                "subscribe"
-              ),
-              color: Colors.brown,
-              onPressed: () {
-
-
-               /*
+                  getRandom().listen((event) {
+                    print("here is the value: $event");
+                  });
+                  /*
                 //added stream listener....
                 Stream stream = controller.stream;
                streamSubscription = stream.listen((event) {
@@ -47,46 +44,35 @@ class _MyAppState extends State<MyApp> {
 
                 });
 */
-            },
-
-            ),
+                },
+              ),
 
               MaterialButton(
-                  child: Text(
-                    "show print-log"
-                  ),
+                  child: Text("show print-log"),
                   color: Colors.blue,
-                  onPressed: (){
-
+                  onPressed: () {
                     //add value to stream
                     controller.add(13);
-
                   }),
 
               //managing your streams to avoid memory leaks
               MaterialButton(
-                color: Colors.red,
-                  child: Text(
-                    "UnSubscribe"
-                  ),
-                  onPressed: (){
-                  streamSubscription.cancel();
-
+                  color: Colors.red,
+                  child: Text("UnSubscribe"),
+                  onPressed: () {
+                    streamSubscription.cancel();
                   })
-         ] ),
-
-        ),
-      )
-    );
+            ]),
+          ),
+        ));
   }
 }
 
-Stream<double>getRandom()async*{
+Stream<double> getRandom() async* {
   var random = Random();
 
-  while(true) {
+  while (true) {
     await Future.delayed(Duration(seconds: 2));
     yield random.nextDouble();
   }
-
 }
